d261e143d2b424394d8d3f8bfaa7bd4b50aff105
Arnoldas Armonas <armonas.arnoldas@gmail.com>
2019-04-11 20:52:34 +0300

    Show main graph when secondary graphs do not handle back action

--- nav/src/main/java/com/arnrmn/nav/Navigator.kt
+++ nav/src/main/java/com/arnrmn/nav/Navigator.kt
@@ -9,6 +9,7 @@ class Navigator(
     @IdRes private val containerViewId: Int,
     private val fragmentManager: FragmentManager
 ) {
+    private var mainGraph: Int = NOT_SET
 
     fun initNavigation(vararg graphs: Graph) {
         fragmentManager.beginTransaction()
@@ -16,14 +17,19 @@ class Navigator(
                 graphs.forEachIndexed { index, graph ->
                     val host = NavHostFragment.create(graph.graphId, graph.arguments)
                     transaction.add(containerViewId, host, graph.graphId.toString())
-                    if (index == 0) transaction.show(host) else transaction.hide(host)
+                    if (index == 0) {
+                        mainGraph = graph.graphId
+                        transaction.show(host)
+                    } else {
+                        transaction.hide(host)
+                    }
                 }
             }.commit()
     }
 
     /**
      * Shows navigation host fragment with provided graph id if such exists.
-     * */
+     */
     fun show(@NavigationRes graphId: Int): Boolean {
         val visible = getVisible()
         val target = getHost(graphId)
@@ -41,7 +47,17 @@ class Navigator(
     /**
      * Returns true when consumes back action, false otherwise.
      */
-    fun navigateUp(): Boolean = getVisible()?.navController?.navigateUp() ?: false
+    fun navigateUp(): Boolean {
+        return getVisible()?.let { visibleHost ->
+            if (visibleHost.tag == mainGraph.toString()) {
+                visibleHost.navController.navigateUp()
+            } else if (!visibleHost.navController.navigateUp()) {
+                show(mainGraph)
+            } else {
+                true
+            }
+        } ?: false
+    }
 
     private fun getHost(@NavigationRes graphId: Int): NavHostFragment? =
         fragmentManager.findFragmentByTag(graphId.toString()) as? NavHostFragment
@@ -50,4 +66,8 @@ class Navigator(
         fragmentManager.fragments
             .filterIsInstance<NavHostFragment>()
             .firstOrNull(NavHostFragment::isVisible)
+
+    companion object {
+        private const val NOT_SET = -1
+    }
 }
